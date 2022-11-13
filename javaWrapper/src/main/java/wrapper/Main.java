ec966619bed2098fa4d6e568f2c9e1da196c44b9
denniswiersma <dennis.wiersma@icloud.com>
2022-11-13 23:33:33 +0100

    added javadoc

--- javaWrapper/src/main/java/wrapper/Main.java
+++ javaWrapper/src/main/java/wrapper/Main.java
@@ -3,6 +3,12 @@ package wrapper;
 import wrapper.WekaReader;
 
 public class Main {
+
+    /**
+     * Entry point for the program. Simply creates an instance of WekaReader.
+     * @param args arguments from the command line.
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         WekaReader.main(args);
     }
--- javaWrapper/src/main/java/wrapper/WekaReader.java
+++ javaWrapper/src/main/java/wrapper/WekaReader.java
@@ -9,22 +9,33 @@ import weka.core.SerializationHelper;
 import weka.core.converters.ConverterUtils.DataSource;
 
 class WekaReader {
-    // final String modelFile = "j48.model";
-    // final String unknownFile = "unknown_weather.arff";
 
-
+    /**
+     * Initialises the class and calls the start method.
+     * @param args arguments from the command line.
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        // STEP 2: load a model and use it
+
         new WekaReader().start(args);
     }
 
+    /**
+     * Starts up the program by loading the ARFF file with instances to classify and
+     * loads the classifier
+     * @param args arguments from the command line.
+     */
     private void start(String[] args) {
         String dataFile = args[0];
         System.out.println("Data File loaded:" + dataFile);
         try {
+            // load the classifier
             AbstractClassifier fromFile = loadClassifier();
+            // load the ARFF with unknown instances
             Instances unknownInstances = loadArff(dataFile);
+            // set the class variable to be the last one in the dataset
             unknownInstances.setClassIndex(unknownInstances.numAttributes() - 1);
+            // classify new instances
             classifyNewInstance(fromFile, unknownInstances);
 
         } catch (Exception e) {
@@ -34,9 +45,9 @@ class WekaReader {
 
 
     private void classifyNewInstance(AbstractClassifier model, Instances unknownInstances) throws Exception {
-        // create copy
+        // create copy of the new instances
         Instances labeled = new Instances(unknownInstances);
-        // label instances
+        // label instances according to the model's prediction
         for (int i = 0; i < unknownInstances.numInstances(); i++) {
             double clsLabel = model.classifyInstance(unknownInstances.instance(i));
             labeled.instance(i).setClassValue(clsLabel);
